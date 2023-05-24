06d26aeaf0fed92da657eaefbf7c0cd154585030
gbuxo <gerard.buxo@gmail.com>
2023-05-24 20:40:24 +0200

    PublicTransports function defined

--- transportsBCN.py
+++ transportsBCN.py
@@ -1,22 +1,12 @@
 import pandas as pd
 import os
-import urllib.request
+# import urllib.request
 from haversine import haversine, Unit # per calcular distancies (m) amb coordenades
 
 """ Descàrrega de dades d'estacions de transport a barcelona """
 
-# https://opendata-ajuntament.barcelona.cat/data/ca/dataset/transports
-
-url = "https://opendata-ajuntament.barcelona.cat/resources/bcn/GuiaBCN/TRANSPORTS.csv"
-archivo_destino = "./Data/TRANSPORTS.csv"
-
-urllib.request.urlretrieve(url, archivo_destino) # si l'arxiu existeix el remplaça
-
-df_trans = pd.read_csv('./Data/TRANSPORTS.csv')
-global_df = pd.read_csv('./Data/global_df.csv')
-
-# dataframe amb els ids de les estacions i coordenades. Per que sigui més rápid el procés
-estacions_coords = global_df[['station_id','lat','lon']].drop_duplicates()
+# info: https://opendata-ajuntament.barcelona.cat/data/ca/dataset/transports
+# global_df = pd.read_csv('./Data/global_df.csv')
 
 def count_trans_within_radius(row, est_bicing, radi):
     """
@@ -27,8 +17,19 @@ def count_trans_within_radius(row, est_bicing, radi):
                  for _, p in est_bicing.iterrows()]
     return len([d for d in distances if d <= radi])
 
-estacions_coords['n_transp_500m'] = estacions_coords.apply(count_trans_within_radius, args=(df_trans, 500), axis=1)
+def PublicTransports(global_df):
+    """
+    descarrega de dades d'estacions de transports publics
+    + calcul num d'estacions a 500m
+    + merge amb global_df
+    """
+    url = "https://opendata-ajuntament.barcelona.cat/resources/bcn/GuiaBCN/TRANSPORTS.csv"
+    df_trans = pd.read_csv(url)
+    estacions_coords = global_df[['station_id','lat','lon']].drop_duplicates()
+    estacions_coords['n_transp_500m'] = estacions_coords.apply(count_trans_within_radius, args=(df_trans, 500), axis=1)
+    global_df = global_df.merge(estacions_coords, on=['station_id','lat','lon'], how='left')
+    return global_df
 
-# global_df_merged = global_df.merge(estacions_coords, on=['station_id','lat','lon'], how='left')
-estacions_coords.to_csv('.Data/bicing_transports.csv', index=False)
 
+# test = PublicTransports(global_df)
+# print(test.head())
