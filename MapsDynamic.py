cc5e3b7f734390da65d1302a7233219326ba57e4
jotogi <toset.j@gmail.com>
2023-06-27 17:58:08 +0200

    Modificació dels maps i estacions.

--- MapsDynamic.py
+++ MapsDynamic.py
@@ -38,10 +38,11 @@ app.layout = html.Div([
             html.Div(
                 [
                     html.Label("Select feature:"),
-                    dcc.Dropdown(options=['Percentage docks available',
+                    dcc.Dropdown(options=['Docks available (%)',
                                           '# of bikes leaved',
                                           '# of bikes taken'],
-                                 value='Percentage docks available',
+                                 value='Docks available (%)',
+                                 clearable=False,
                                  id='feature-selection',
                                  className='dropdown-class')                    
                 ],
@@ -53,6 +54,7 @@ app.layout = html.Div([
                     html.Label("Avoid until hour:"),
                     dcc.Dropdown(options=list(range(0,24)),
                                  value=0,
+                                 clearable=False,
                                  id='hour-selection',
                                  className='dropdown-class')                    
                 ],
@@ -73,11 +75,13 @@ app.layout = html.Div([
 def display_animated_graph(date_value, feature_value, hour_clean):
     year, month, day = date_value.split('-')
     year_, month_, day_ = int(year), int(month), int(day)
-    features = {'Percentage docks available':'percentage_docks_available',
-                '# of bikes leaved':'number_of_bikes_leaved',
-                '# of bikes taken':'number_of_bikes_taken',
+    features = {'Docks available (%)':['percentage_docks_available',1],
+                '# of bikes leaved':['number_of_bikes_leaved',100],
+                '# of bikes taken':['number_of_bikes_taken',100],
                 }
-    z_feature = features[feature_value]
+    z_feature = features[feature_value][0]
+    max_z_color_scale = features[feature_value][1]
+
     # month_name = {
     #     1:'Gener',2:'Febrer',3:'Març',4:'Abril',5:'Maig',6:'Juny',
     #     7:'Juliol',8:'Agost',9:'Setembre',10:'Octubre',11:'Novembre',12:'Desembre',
@@ -93,12 +97,19 @@ def display_animated_graph(date_value, feature_value, hour_clean):
                 (df_to_read['day']==day_)
                 )
     df_to_read = df_to_read[filter_df]
+    # max_z_color_scale = df_to_read[z_feature].max()
+
     animation = px.density_mapbox(df_to_read,
                             lat='lat', lon='lon', z=z_feature,#z='percentage_docks_available',
                             radius=8,
                             center=dict(lat=41.40, lon=2.17),
                             zoom=12,
                             opacity=1.0,
+                            range_color = [0,max_z_color_scale],
+                            color_continuous_scale='inferno',
+                            # color_continuous_scale=[(0.00, "blue"),   (0.33, "blue"),
+                            #                         (0.33, "green"), (0.66, "green"),
+                            #                         (0.66, "red"),  (1.00, "red")],
                             mapbox_style='open-street-map',
                             height = 1000,
                             animation_frame= 'Hour:Minute',
--- Stations.py
+++ Stations.py
@@ -34,8 +34,6 @@ def iterate_over_stations(df:pd.DataFrame, hour:int=0)->pd.DataFrame:
         df_fixed_station['diff-dec'] = df_fixed_station['num_bikes_available'].diff(1)
         df_fixed_station['diff-inc'] = df_fixed_station['diff-inc'].apply(lambda x: 0 if x<0 else x)
         df_fixed_station['diff-dec'] = df_fixed_station['diff-dec'].apply(lambda x: 0 if x>0 else -x)
-        # df_fixed_station['diff-inc'].iloc[0] = 0
-        # df_fixed_station['diff-dec'].iloc[0] = 0
         df_fixed_station['number_of_bikes_leaved'] = df_fixed_station['diff-inc'].cumsum()
         df_fixed_station['number_of_bikes_taken'] = df_fixed_station['diff-dec'].cumsum()
 
@@ -47,17 +45,50 @@ def iterate_over_stations(df:pd.DataFrame, hour:int=0)->pd.DataFrame:
     df = df.fillna(0)
     return df
 
-    
-     
+def iterate_over_days(df:pd.DataFrame, hour:int=0)->pd.DataFrame:
+    dataframes_list = []
+    for day in list(df['day'].unique()):
+        df_fixed_station = df[(df['day']==day) & (df['hour']>=hour)][['num_bikes_available']].copy()
+        df_fixed_station['number_of_bikes_leaved'] = 0
+        df_fixed_station['number_of_bikes_taken'] = 0
+        df_fixed_station['diff-inc'] = df_fixed_station['num_bikes_available'].diff(1)
+        df_fixed_station['diff-dec'] = df_fixed_station['num_bikes_available'].diff(1)
+        df_fixed_station = df_fixed_station.fillna(0)
+        df_fixed_station['diff-inc'] = df_fixed_station['diff-inc'].apply(lambda x: 0 if x<=0 else x)
+        df_fixed_station['diff-dec'] = df_fixed_station['diff-dec'].apply(lambda x: 0 if x>=0 else -x)
+        df_fixed_station['number_of_bikes_leaved'] = df_fixed_station['diff-inc'].cumsum()
+        df_fixed_station['number_of_bikes_taken'] = df_fixed_station['diff-dec'].cumsum()
+
+        dataframes_list.append(df_fixed_station)
+
+    df_fixed_station_concatenated = pd.concat(dataframes_list, axis=0)
+    # logger_stations.debug(f'{df_fixed_station_concatenated.columns}')
+    df = df.merge(df_fixed_station_concatenated, left_index=True, right_index=True )
+    df = df.fillna(0)
+    return df
 
 def get_df_for_dynamics(year:int, month:int, day:int, hour:int=0):
     df_initial = prepare_df_from_csv_file(year, month, day)
     return iterate_over_stations(df_initial, hour)
 
-def main():
-    result = get_df_for_dynamics(2021,11,8)
-    print(result.columns)
+def all_stations_analisys(df:pd.DataFrame, columns_to_include)->pd.DataFrame:
+    grouped_df = df[columns_to_include].groupby(['year','month','day','hour','minute']).agg(sum)    
+    return grouped_df
 
+def main():
+    df_pre = pd.read_csv(f'./Data/STATIONS_CLEANED/PRE_2022_06_Juny_BicingNou_ESTACIONS.csv')
+    df_pre.drop_duplicates(inplace=True)
+    columns_to_group = ['year', 'month', 'day', 'hour', 'minute', 'last_updated','weekend']
+    columns_to_filter = columns_to_group + ['num_bikes_available', 'num_docks_available','capacity']
+    df_pre_alternative = df_pre[columns_to_filter].groupby(columns_to_group).agg(num_bikes_available=('num_bikes_available','sum'),
+                                                                             num_docks_available=('num_docks_available','sum'),
+                                                                             capacity=('capacity','sum')).copy()
+    df_pre_alternative.reset_index(inplace=True)
+    df_pre_alternative['Hour:Minute']=df_pre_alternative['hour']+df_pre_alternative['minute']/60
+    df_pre_alternative['Hour:Minute']=df_pre_alternative['Hour:Minute'].round(2)
+    df_fixed_day =  iterate_over_days(df_pre_alternative)
+    df_fixed_day['bike_balance'] = df_fixed_day['number_of_bikes_taken'] - df_fixed_day['number_of_bikes_leaved']
+    print(df_fixed_day['number_of_bikes_taken'].max())
 
 if __name__ == '__main__':
     main()
